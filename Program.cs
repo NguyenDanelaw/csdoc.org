52ce5336ade2d77a2495182b0282134f5b3821df
Nguyễn Danelaw <davepopeditor2@gmail.com>
2025-03-11 12:48:00 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-int[] numbers = {5, 3, 7, 2, 8};
-Console.WriteLine(numbers.Length);
-Array.Sort(numbers);
-Console.WriteLine(string.Join(",", numbers));
+using System;
+
+namespace HelloWorld {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
